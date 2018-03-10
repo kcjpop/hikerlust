f328fa4f9b27e0bc89d1b38c0fe3a1b8789a21ae
An Cao <kcj.pop.com@gmail.com>
2018-03-10 07:15:05 +0200

    Make location list accordion
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- src/pages/ban-do-lang-thang.js
+++ src/pages/ban-do-lang-thang.js
@@ -7,113 +7,193 @@ import { formatPostDate } from '@/helpers/fecha'
 import countTagPost from '@/helpers/countTagPost'
 
 const MAP_STYLES = [
-  { elementType: 'geometry', stylers: [{ color: '#ebe3cd' }] },
-  { elementType: 'labels.text.fill', stylers: [{ color: '#523735' }] },
-  { elementType: 'labels.text.stroke', stylers: [{ color: '#f5f1e6' }] },
   {
-    featureType: 'administrative',
-    elementType: 'geometry.stroke',
-    stylers: [{ color: '#c9b2a6' }]
-  },
-  {
-    featureType: 'administrative.land_parcel',
-    elementType: 'geometry.stroke',
-    stylers: [{ color: '#dcd2be' }]
-  },
-  {
-    featureType: 'administrative.land_parcel',
-    elementType: 'labels.text.fill',
-    stylers: [{ color: '#ae9e90' }]
-  },
-  {
-    featureType: 'landscape.natural',
+    featureType: 'water',
     elementType: 'geometry',
-    stylers: [{ color: '#dfd2ae' }]
+    stylers: [
+      {
+        visibility: 'on'
+      },
+      {
+        color: '#aee2e0'
+      }
+    ]
   },
   {
-    featureType: 'poi',
-    elementType: 'geometry',
-    stylers: [{ color: '#dfd2ae' }]
-  },
-  {
-    featureType: 'poi',
-    elementType: 'labels.text.fill',
-    stylers: [{ color: '#93817c' }]
-  },
-  {
-    featureType: 'poi.park',
+    featureType: 'landscape',
     elementType: 'geometry.fill',
-    stylers: [{ color: '#a5b076' }]
+    stylers: [
+      {
+        color: '#abce83'
+      }
+    ]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#769E72'
+      }
+    ]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#7B8758'
+      }
+    ]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#EBF4A4'
+      }
+    ]
   },
   {
     featureType: 'poi.park',
-    elementType: 'labels.text.fill',
-    stylers: [{ color: '#447530' }]
+    elementType: 'geometry',
+    stylers: [
+      {
+        visibility: 'simplified'
+      },
+      {
+        color: '#8dab68'
+      }
+    ]
   },
   {
     featureType: 'road',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        visibility: 'simplified'
+      }
+    ]
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#5B5B3F'
+      }
+    ]
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#ABCE83'
+      }
+    ]
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'off'
+      }
+    ]
+  },
+  {
+    featureType: 'road.local',
     elementType: 'geometry',
-    stylers: [{ color: '#f5f1e6' }]
+    stylers: [
+      {
+        color: '#A4C67D'
+      }
+    ]
   },
   {
     featureType: 'road.arterial',
     elementType: 'geometry',
-    stylers: [{ color: '#fdfcf8' }]
+    stylers: [
+      {
+        color: '#9BBF72'
+      }
+    ]
   },
   {
     featureType: 'road.highway',
     elementType: 'geometry',
-    stylers: [{ color: '#f8c967' }]
+    stylers: [
+      {
+        color: '#EBF4A4'
+      }
+    ]
   },
   {
-    featureType: 'road.highway',
+    featureType: 'transit',
+    stylers: [
+      {
+        visibility: 'off'
+      }
+    ]
+  },
+  {
+    featureType: 'administrative',
     elementType: 'geometry.stroke',
-    stylers: [{ color: '#e9bc62' }]
+    stylers: [
+      {
+        visibility: 'on'
+      },
+      {
+        color: '#87ae79'
+      }
+    ]
   },
   {
-    featureType: 'road.highway.controlled_access',
-    elementType: 'geometry',
-    stylers: [{ color: '#e98d58' }]
-  },
-  {
-    featureType: 'road.highway.controlled_access',
-    elementType: 'geometry.stroke',
-    stylers: [{ color: '#db8555' }]
-  },
-  {
-    featureType: 'road.local',
-    elementType: 'labels.text.fill',
-    stylers: [{ color: '#806b63' }]
-  },
-  {
-    featureType: 'transit.line',
-    elementType: 'geometry',
-    stylers: [{ color: '#dfd2ae' }]
-  },
-  {
-    featureType: 'transit.line',
-    elementType: 'labels.text.fill',
-    stylers: [{ color: '#8f7d77' }]
-  },
-  {
-    featureType: 'transit.line',
-    elementType: 'labels.text.stroke',
-    stylers: [{ color: '#ebe3cd' }]
-  },
-  {
-    featureType: 'transit.station',
-    elementType: 'geometry',
-    stylers: [{ color: '#dfd2ae' }]
-  },
-  {
-    featureType: 'water',
+    featureType: 'administrative',
     elementType: 'geometry.fill',
-    stylers: [{ color: '#b9d3c2' }]
+    stylers: [
+      {
+        color: '#7f2200'
+      },
+      {
+        visibility: 'off'
+      }
+    ]
   },
   {
-    featureType: 'water',
+    featureType: 'administrative',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        color: '#ffffff'
+      },
+      {
+        visibility: 'on'
+      },
+      {
+        weight: 4.1
+      }
+    ]
+  },
+  {
+    featureType: 'administrative',
     elementType: 'labels.text.fill',
-    stylers: [{ color: '#92998d' }]
+    stylers: [
+      {
+        color: '#495421'
+      }
+    ]
+  },
+  {
+    featureType: 'administrative.neighborhood',
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off'
+      }
+    ]
   }
 ]
 
@@ -190,6 +270,10 @@ const Map = withScriptjs(
 )
 
 class LocationAccordion extends React.Component {
+  state = {
+    expanded: ''
+  }
+
   sortPlaces(places) {
     return places.reduce((acc, { node }) => {
       const country = acc[node.country] || []
@@ -201,6 +285,19 @@ class LocationAccordion extends React.Component {
     return subplaces.reduce((acc, place) => acc + countTagPost(place), 0)
   }
 
+  isExpanded = country => {
+    return this.state.expanded === country
+  }
+
+  getCountryListIcon = country => {
+    return this.isExpanded(country) ? <i className="fa fa-angle-down" /> : <i className="fa fa-angle-right" />
+  }
+
+  doToggleExpand = country => e => {
+    e.preventDefault()
+    this.setState({ expanded: this.isExpanded(country) ? '' : country })
+  }
+
   render() {
     const places = this.sortPlaces(this.props.places)
 
@@ -209,16 +306,20 @@ class LocationAccordion extends React.Component {
         {Object.entries(places).map(([country, subplaces]) => {
           return (
             <div className="mb3" key={country}>
-              <h3 className="pointer f4 gold fw3 ma0 pa0">
-                <i className="fa fa-angle-right" />
+              <button
+                type="button"
+                className="outline-0 db bn bg-transparent pointer f4 gold fw3 ma0 pa0"
+                onClick={this.doToggleExpand(country)}
+              >
+                <span className="dib w1 tc">{this.getCountryListIcon(country)}</span>
                 <span className="pl2 inline-flex items-center" title="bài viết">
                   {country}
                   <small className="ml1 w1 h1 br-100 bg-gold white f6 inline-flex items-center justify-center">
                     {this.countPosts(subplaces)}
                   </small>
                 </span>
-              </h3>
-              <ul className="list pl3">
+              </button>
+              <ul className={classnames('list', { clip: !this.isExpanded(country) })} style={{ paddingLeft: '1.5rem' }}>
                 {subplaces.map(place => (
                   <li key={place.id} onClick={this.props.doCenterMap(place)} className="db mv3 pointer">
                     {place.name}
@@ -276,7 +377,7 @@ class HikerMap extends React.Component {
         <LocationAccordion
           doCenterMap={this.doCenterMap}
           places={data.places.edges}
-          className="absolute top-1 left-1 z-2 pv3 ph4 bg-white"
+          className="absolute top-1 left-1 z-2 pv3 ph4 bg-white w5"
         />
         <Map
           mapRef={el => (this.mapRef = el)}
@@ -286,7 +387,7 @@ class HikerMap extends React.Component {
           places={data.places.edges}
           googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDt8lqo3-UTGbIZZef9zKmVMdPXm2rnLjY"
           loadingElement={<div style={{ height: '100%' }} />}
-          containerElement={<div style={{ height: '80vh' }} />}
+          containerElement={<div style={{ height: '85vh' }} />}
           mapElement={<div style={{ height: '100%' }} />}
         />
       </div>
