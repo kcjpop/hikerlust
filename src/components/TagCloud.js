747581ab21ae74c7b632bb2e7569c67316ec9efe
An Cao <kcj.pop.com@gmail.com>
2018-03-14 07:56:43 +0200

    Fix count tag not having any post
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- src/components/TagCloud.js
+++ src/components/TagCloud.js
@@ -23,7 +23,7 @@ export default function(props) {
       {props.tags.edges.map(({ node }) => (
         <li className="dib" key={node.id}>
           <Link to={`/tag/${node.slug}`} className="db pv2 ph3 mr2 mb2 ba b--gold gold flex items-center">
-            {node.title}{' '}
+            {node.title}
             <span className="ml1 w1 h1 br-100 bg-gold white f6 inline-flex items-center justify-center">
               {countTagPost(node)}
             </span>
--- src/helpers/countTagPost.js
+++ src/helpers/countTagPost.js
@@ -1,3 +1,3 @@
 export default function(tag) {
-  return new Set(Array.from(tag.post, post => post.id)).size
+  return tag.post != null ? new Set(Array.from(tag.post, post => post.id)).size : 0
 }
