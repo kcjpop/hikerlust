1f07716f50249eca9f4ee4cb2eaec715a4882379
An Cao <kcj.pop.com@gmail.com>
2018-03-28 17:30:15 +0300

    Update spaces in PostList
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- src/components/BigBanner.js
+++ src/components/BigBanner.js
@@ -7,7 +7,7 @@ export default function(props) {
   return (
     <div className="relative mt3-ns mb4-ns cover bg-center" style={{ backgroundImage: `url(${bgImage})` }}>
       <div className="absolute top-0 left-0 right-0 bottom-0 z1 bg-black-50" />
-      <Link to={href} className="relative tc db pv7-ns z2">
+      <Link to={href} className="relative tc db pv6-ns z2">
         <img src={logo} alt={title} className="pa3 pa0-ns mw5 mw6-ns" />
       </Link>
     </div>
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -10,9 +10,9 @@ export default function(props) {
       <div className="flex flex-wrap mt3">
         {posts.map((post, index) => (
           <Post
-            className={classnames('w-50-ns lh-copy tc pt3 pb2 bb bn-ns b--light-gray no-last-border', {
-              'pr2-ns': index % 2 === 0,
-              'pl2-ns': index % 2 !== 0
+            className={classnames('w-50-ns lh-copy tc pt3 pb3 bb bn-ns b--light-gray no-last-border', {
+              'pr3-ns': index % 2 === 0,
+              'pl3-ns': index % 2 !== 0
             })}
             key={post.node.id}
             post={post.node}
