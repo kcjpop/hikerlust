72224143cf1bf51fb7ee80012a2464b7e519fb6f
An Cao <kcj.pop.com@gmail.com>
2018-04-11 18:44:07 +0300

    Fix BigBanner layout on non-mobile
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- src/components/BigBanner.js
+++ src/components/BigBanner.js
@@ -5,7 +5,7 @@ import logo from '@/layouts/img/logo.png'
 export default function(props) {
   const { bgImage, href, title } = props
   return (
-    <div className="h5 relative mb4-ns cover bg-center" style={{ backgroundImage: `url(${bgImage})` }}>
+    <div className="h5 h-auto-ns relative mb4-ns cover bg-center" style={{ backgroundImage: `url(${bgImage})` }}>
       <div className="absolute top-0 left-0 right-0 bottom-0 z1 bg-black-50" />
       <Link to={href} className="clip-s relative tc db z2 flex items-center justify-center big-banner-height">
         <img src={logo} alt={title} className="pa3 pa0-ns mw5 mw6-ns" />
