0486ed0b1098bc7e34573beaebdaf94e9a8e2a2c
An Cao <kcj.pop.com@gmail.com>
2018-04-12 16:13:48 +0300

    Update 404 page
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Link from 'gatsby-link'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div className="mv3 mv5-ns tc">
+    <h1 className="f2">Đường dẫn không tồn tại :(</h1>
+    <p className="lh-copy f5">
+      Hikerlust đang trong quá trình chuyển đổi sang hệ thống mới nên có thể đường dẫn cũ không còn hoạt động nữa.
+    </p>
+    <p className="lh-copy f5">
+      Bạn vui lòng kiểm tra lại hoặc sử dụng chức năng
+      <Link to="/search" className="mh1 ba b--gold pv2 ph3">
+        Tìm Kiếm
+      </Link>
+      trên website.
+    </p>
   </div>
 )
 
