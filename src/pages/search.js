ba9c66e9b5a3f2a28a23a5700a97c3b93b17a73f
An Cao <kcj.pop.com@gmail.com>
2018-04-12 16:31:10 +0300

    Update layout of search results
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- src/pages/search.js
+++ src/pages/search.js
@@ -6,11 +6,14 @@ function Item({ hit }) {
   const { node } = hit
 
   return (
-    <div className="pa3 bb b--light-gray">
-      <Link to={`/${node.slug}`} className="f4 ttu tracked">
-        <Highlight attributeName="node.title" hit={hit} />
-      </Link>
-      <p className="lh-copy">{node.excerpt}</p>
+    <div className="pa3 bb b--light-gray flex items-stretch">
+      <div className="w-20 h4 cover" style={{ backgroundImage: `url(${node.featuredImage})` }} />
+      <div className="w-80 pa3">
+        <Link to={`/${node.slug}`} className="f4 ttu tracked">
+          <Highlight attributeName="node.title" hit={hit} />
+        </Link>
+        <p className="lh-copy">{node.excerpt}</p>
+      </div>
     </div>
   )
 }
