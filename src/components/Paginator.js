fa08b072b9d4a512b3423389c0b197250c1dfd1c
An Cao <kcj.pop.com@gmail.com>
2018-04-14 12:49:28 +0300

    Change style of next/prev links
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- src/components/Paginator.js
+++ src/components/Paginator.js
@@ -7,12 +7,12 @@ export default function(props) {
   return (
     <div className="flex items-center mt3">
       {first ? null : (
-        <Link to={makePrevPath(props)} className="ph3 pv2 ba b--gold br2 gold">
+        <Link to={makePrevPath(props)} className="ph3 pv2 ba b--gold ttu gold">
           &laquo; Trang trước
         </Link>
       )}
       {last ? null : (
-        <Link to={makeNextPath(props)} className="ml-auto ph3 pv2 ba b--gold br2 gold">
+        <Link to={makeNextPath(props)} className="ml-auto ph3 pv2 ba b--gold ttu gold">
           Trang sau &raquo;
         </Link>
       )}
